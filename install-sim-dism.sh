378ed84d195f297236907181ee55854193daa547
sawyermade <sawyermade@gmail.com>
2017-08-25 14:13:37 -0400

    fixed for ubuntu gnome

--- install-sim-dism.sh
+++ install-sim-dism.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-[[ ":$PATH:" != *":$HOME/.local/bin:"* ]] && PATH="$HOME/.local/bin:${PATH}" && echo "export PATH=$HOME/.local/bin:$PATH" >> $HOME/.bashrc
+#[[ ":$PATH:" != *":$HOME/.local/bin:"* ]] && PATH="${HOME}/.local/bin:${PATH}" && echo 'export PATH=${HOME}/.local/bin:${PATH}' >> $HOME/.bashrc
+if [[ ":${PATH}:" != *":$HOME/.local/bin:"* ]]; then
+	echo "Adding $HOME/.local/bin to PATH"
+	export PATH=${HOME}/.local/bin:${PATH}
+	echo 'export PATH=${HOME}/.local/bin:${PATH}' >> $HOME/.bashrc
+fi
+
 sh sim-dism-deps.sh
 sh sim-dism-dlcomp.sh
 cp sim-dism/sim-dism $HOME/.local/bin
