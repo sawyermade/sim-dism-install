1e305dfdfa94a12aece90006ae36098febea79fe
Daniel Sawyer <sawyermade@gmail.com>
2017-08-26 23:16:38 -0400

    fixed stuff

--- sim-dism-deps.sh
+++ sim-dism-deps.sh
@@ -3,6 +3,7 @@ sudo apt install flex gcc-4.7 -y
 wget ftp://ftp.gnu.org/gnu/bison/bison-2.4.1.tar.gz
 tar zxf bison-2.4.1.tar.gz
 cd bison-2.4.1
+mkdir $HOME/.local/
 ./configure --prefix=$HOME/.local/
 make
 make install
